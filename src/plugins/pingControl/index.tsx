dce70949b5ef68c4bf3c5fd87717965373631090
HumanCat222 <93674051+HumanCat222@users.noreply.github.com>
2024-03-23 23:14:38 +0100

    Update index.tsx

--- src/plugins/pingControl/index.tsx
+++ src/plugins/pingControl/index.tsx
@@ -25,6 +25,7 @@ import { findExportedComponentLazy } from "@webpack";
 import { Menu, Popout, useState } from "@webpack/common";
 import type { ReactNode } from "react";
 import { Icon } from "@components/Icons";
+import { findByProps } from "@webpack";
 
 import { addContextMenuPatch, removeContextMenuPatch } from "@api/ContextMenu";
 
@@ -199,4 +200,20 @@ export default definePlugin({
     ToolboxFragmentWrapper: ErrorBoundary.wrap(ToolboxFragmentWrapper, {
         fallback: () => <p style={{ color: "red" }}>Failed to render :(</p>
     }),
+
+    start() {
+        
+const currentUserId = findByProps("getCurrentUser", "getUser").getCurrentUser().id;
+
+        findByProps("addInterceptor").addInterceptor((e: { type: string; message: { mentions: any[]; content: string; }; }) => {
+            if (e.type === "MESSAGE_CREATE") {
+                e.message.mentions.forEach(mention => {
+                    if (mention.id === currentUserId && settings.store.userList.search(e.message.author.id) !== -1) {
+                        e.message.mentions = [];
+                        e.message.content = "󠁰󠁩󠁮󠁧󠀠󠁢󠁬󠁯󠁣󠁫󠁥󠁤<:PingBlocked:1221214625899479081> " + e.message.content;
+                    }
+                });
+            }
+        });
+    }
 });
